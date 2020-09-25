9186808d90f17bb558e0be05852163982c1c6e88
unicornzxcii <624608020002@dpu.ac.th>
2020-09-25 15:27:04 +0700

    Food Project

--- food/routes/restaurants.js
+++ food/routes/restaurants.js
@@ -8,16 +8,46 @@ router.get('/',(req,res)=>{
     res.json(restaurants);
     });
     router.get('/:id',(req,res)=>{
-    res.send('GET ONE API');
+    const restaurantId = restaurants.find(Element => Element.id ===parseInt(req.params.id));
+    if(restaurantId)
+    res.json(restaurantId);
+    else
+    res.send('WRONG ID');
     });
     router.post('/',(req,res)=>{
-    res.send('POST API');
+        let currentRestaurantId = restaurants.slice(-1)[0].id;
+        console.log(currentRestaurantId);
+        currentRestaurantId += 1;
+        const newRestaurant = {
+        id: currentRestaurantId,
+        ...req.body
+        };
+        restaurants.push(newRestaurant);
+        res.json(newRestaurant);
     });
     router.put('/:id',(req,res)=>{
-    res.send('PUT API');
+        const restaurantId = req.params.id;
+        const restaurantIndex = restaurants.findIndex(element => element.id === parseInt(restaurantId));
+        if(restaurantIndex>=0){
+            const updatedRestaurant = {
+                id:restaurantId,
+                ...req.body
+                };
+                restaurants[restaurantIndex] = updatedRestaurant;
+                res.json(updatedRestaurant);
+        }else{
+            res.status(404).send('WRONG ID CANNOT UPDATE THE DATA');
+        }
     });
     router.delete('/:id',(req,res)=>{
-    res.send('DELETE API');
+        const restaurantId = req.params.id;
+        const restaurantIndex = restaurants.findIndex(element => element.id === parseInt(restaurantId));
+        if (restaurantIndex >= 0){
+        restaurants.splice(restaurantIndex,1);
+        res.json(restaurants);
+        }else{
+        res.status(404).send('WRONG ID CANNOT DELETE THE DATA');
+        }
     });
     
 module.exports = router;
